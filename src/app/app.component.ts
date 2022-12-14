edb52cd54ee291a818bbf8fe6f048a4256f941b4
Mrcsbda <macbedoya@gmail.com>
2022-12-14 10:40:37 -0500

    result

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
+  x = 'hola mundo';
   theme1: boolean = true;
   theme2: boolean = false;
   theme3: boolean = false;
